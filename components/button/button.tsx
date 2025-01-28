6aba64a1c305be0aeca8f80e2a2b48bd7d3e362c
comradeskolopendra <comradeskolopendra@gmail.com>
2025-01-28 22:53:02 +0700

    feat: Добавил svgr

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,19 +1,29 @@
 import clsx from "clsx";
 import { FC } from "react";
 
+import ArrowIcon from "./assets/arrow.svg";
 import styles from "./button.module.css";
 import { ButtonProps } from "./button.props";
 
 
-export const Button: FC<ButtonProps> = ({children, appearance, onClick}) => {
+export const Button: FC<ButtonProps> = ({children, appearance, arrow = "none", className, ...rest}) => {
 
     return (
         <button 
-            onClick={onClick} 
-            className={clsx(styles.button, {
+            className={clsx(styles.button, className, {
                 [styles.primary]: appearance === "primary",
                 [styles.ghost]: appearance === "ghost"
             })}
-        >{children}</button>
+            {...rest}
+        >
+            {children}
+            {arrow !== "none" && 
+                <span className={clsx(styles.arrow, {
+                    [styles.down]: arrow === "down"
+                })}>
+                    <ArrowIcon/>
+                </span>
+            }
+        </button>
     )
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <Htag tag="h2">Текст</Htag>
       <Htag tag="h3">Текст</Htag>
 
-      <Button appearance={"primary"}>Button</Button>
-      <Button appearance={"ghost"}>Button</Button>
+      <Button appearance={"primary"} arrow="down">Button</Button>
+      <Button appearance={"ghost"} arrow="right">Button</Button>
     </>
   );
 }
