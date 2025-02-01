38c6cd2e16a25af87161c7f29ca6a80d79057623
comradeskolopendra <comradeskolopendra@gmail.com>
2025-02-02 00:00:51 +0700

    feat: Сверстал footer

--- layout/footer/footer.tsx
+++ layout/footer/footer.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
 import { FooterProps } from "./footer.props";
 
+import clsx from "clsx";
+import styles from "./footer.module.css";
 
-export const Footer: FC<FooterProps> = ({...props}) => {
+
+export const Footer: FC<FooterProps> = ({className, ...props}) => {
     return (
-        <footer {...props}>
-            footer
+        <footer className={clsx(className, styles.footer)} {...props}>
+            <div>OwlTop © 2020 - {new Date().getFullYear()} Все права защищены</div>
+            <a href={"#"} target="_blank">Пользовательское соглашение</a>
+            <a href={"#"} target="_blank">Политика конфиденциальности</a>
         </footer>
     )
 };
