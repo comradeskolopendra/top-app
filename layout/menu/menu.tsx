9ee0132cef3c8cb0dfcbe7a057b476524395c8f0
comradeskolopendra <comradeskolopendra@gmail.com>
2025-07-10 13:46:20 +0700

    refactor: Переверстал меню

--- layout/menu/menu.tsx
+++ layout/menu/menu.tsx
@@ -1,13 +1,11 @@
 import { AppContext } from "@/context/app.context";
-import { IFirstLevelMenuItem, IPageItem } from "@/interfaces/menu.interface";
+import {IFirstLevelMenuItem, IPageItem} from "@/interfaces/menu.interface";
 import { TopLevelCategory } from "@/interfaces/page.interface";
 import { FC, useContext } from "react";
 
 import styles from "./menu.module.css";
 
 import clsx from "clsx";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import BooksSvg from "./assets/books.svg";
 import CoursesSvg from "./assets/courses.svg";
 import ProductsSvg from "./assets/products.svg";
@@ -33,7 +31,7 @@ const firstLevelMenu: IFirstLevelMenuItem[] = [
         id: TopLevelCategory.BOOKS
     },
     {
-        route: "product",
+        route: "products",
         name: "Товары",
         icon: <ProductsSvg />,
         id: TopLevelCategory.PRODUCTS
@@ -43,10 +41,60 @@ const firstLevelMenu: IFirstLevelMenuItem[] = [
 const Menu: FC = () => {
     const {menu, setMenu, firstCategory} = useContext(AppContext);
 
+    const buildFirstLevel = ()=> {
+        return (
+            <>
+                {firstLevelMenu.map(flMenu => (
+                    <div key={flMenu.route}>
+                        <a className={styles.firstLevelLink} href={`/${flMenu.route}`}>
+                            <div className={clsx(styles.firstLevel, {
+                                [styles.firstLevelActive]: flMenu.id === firstCategory
+                            })}>
+                                {flMenu.icon}
+                                <span>{flMenu.name}</span>
+                            </div>
+                        </a>
+
+                        {flMenu.id === firstCategory && buildSecondLevel(flMenu)}
+                    </div>
+                ))}
+            </>
+        )
+    };
+
+    const buildSecondLevel = (flMenu: IFirstLevelMenuItem) => {
+        return (
+            <div className={styles.secondBlock}>
+                {menu.map((menuItem) => (
+                    <div key={menuItem._id.secondCategory}>
+                        <div className={styles.secondLevel}>{menuItem._id.secondCategory}</div>
+                        <div className={clsx(styles.secondLevelBlock, {
+                            [styles.secondLevelBlockOpened]: menuItem.isOpened
+                        })}>
+                            {buildThirdLevel(menuItem.pages, flMenu.route)}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    };
+
+    const buildThirdLevel = (pages: IPageItem[], route: string) => {
+        return (
+                pages.map(page => (
+                    <a href={`/${route }/${page.alias}`} className={clsx(styles.thirdLevel, {
+                        [styles.thirdLevelActive]: false
+                    })}>
+                        {page.category}
+                    </a>
+                ))
+        )
+    };
+
     return (
-        <ul>
-            {menu.map(element => <li>{element._id.secondCategory}</li>)}
-        </ul>
+        <div className={styles.menu}>
+            {buildFirstLevel()}
+        </div>
     )
 };
 
