6d32dfe9e14654b633702c9dca2d42895036e4b7
comradeskolopendra <comradeskolopendra@gmail.com>
2025-07-24 19:00:39 +0700

    feat: Добавил мобильное меню и анимацию на него

--- layout/header/header.tsx
+++ layout/header/header.tsx
@@ -1,11 +1,63 @@
-import { FC } from "react";
+import {FC, useEffect, useState} from "react";
 import { HeaderProps } from "./header.props";
+import clsx from "clsx";
 
-export const Header: FC<HeaderProps> = ({...props}) => {
+import styles from "./header.module.css";
+
+import Logo from "./assets/logo.svg";
+import {IconButton} from "@/components/icon-button/icon-button";
+
+import {motion} from "framer-motion";
+import {Sidebar} from "@/layout/sidebar/sidebar";
+
+import {useRouter} from "next/router";
+
+export const Header: FC<HeaderProps> = ({className, ...props}) => {
+    const [opened, setOpened] = useState(false);
+    const router = useRouter();
+
+    const handleCloseSidebar = () => {
+        setOpened(false);
+    };
+
+    const handleOpenSidebar = () => {
+        setOpened(true);
+    };
+
+    useEffect(() => {
+        setOpened(false);
+    }, [router.asPath]);
+
+    const variants = {
+        "hidden": {
+            translateX: "100%",
+            transition: {
+                stiffness: 20
+            }
+        },
+        "visible": {
+            translateX: 0,
+            transition: {
+                stiffness: 20
+            }
+        }
+    };
 
     return (
-        <header {...props}>
-            Header
+        <header className={clsx(styles.header)} {...props}>
+            <Logo/>
+            <IconButton onClick={handleOpenSidebar} icon={"menu"} appearance={"white"}/>
+
+            <motion.div
+                variants={variants}
+                initial={"hidden"}
+                animate={opened ? "visible" : "hidden"}
+                className={styles.mobileMenu}
+            >
+                <Sidebar/>
+
+                <IconButton className={styles.closeMenu} icon={"close"} onClick={handleCloseSidebar} appearance={"white"}/>
+            </motion.div>
         </header>
     )
 };
